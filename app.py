f3260571190a7c6b816ec7374c9e36bcc2d304fc
Hsin <ydnicnai@gmail.com>
2020-05-17 21:50:40 +0800

    test-=-

--- app.py
+++ app.py
@@ -39,20 +39,23 @@ def callback():
 def handle_message(event):
     msg = event.message.text
 
+    for d in msg :
+        if '感情' or '男朋友' or '女朋友' or '愛情' in d:
+            r = '有關感情的事我一律建議分手'
+            break
 
-    if '感情' or '男朋友' or '女朋友' or '愛情' in msg:
-        r = '有關感情的事我一律建議分手'
-    elif '事業' or '工作' or '上班' in msg:
-        r = '有關工作的事我一律建議辭職'
-    else:
-        sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='1'
-        )
-        line_bot_api.reply_message(
-            event.reply_token,
-            sticker_message)
-        return
+        elif '事業' or '工作' or '上班' in d:
+            r = '有關工作的事我一律建議辭職'
+            break
+        else:
+            sticker_message = StickerSendMessage(
+                package_id='1',
+                sticker_id='1'
+            )
+            line_bot_api.reply_message(
+                event.reply_token,
+                sticker_message)
+            break
 
     line_bot_api.reply_message(
         event.reply_token,
