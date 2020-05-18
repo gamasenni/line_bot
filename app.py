0c26fee33a297dca9efcf38c2dd2247f72e2f4c0
Hsin <ydnicnai@gmail.com>
2020-05-18 11:18:57 +0800

    added more

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def handle_message(event):
                             line_bot_api.reply_message(
                                 event.reply_token,
                                 sticker_message)
-    break
+            break
 
     line_bot_api.reply_message(
         event.reply_token,
